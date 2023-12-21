624d66fe0ba7116708cc63bb0b673854264deefc
abdiHaret <abdiharet68@gmail.com>
2023-12-20 23:14:10 -0500

    Added another program

---
+++ twoNumComp.c
@@ -0,0 +1,16 @@
+/*
+    * This is how multiple line commenting is done in c 
+    * This program will teach you how to use a basic if else statement
+*/
+#include <stdio.h>
+int main(){
+    int num1, num2;
+    printf("Enter 2 numbers : ");
+    scanf("%d %d", &num1, &num2);
+    if(num1 == num2){
+        printf("The 2 numbers are equal\n");
+    }
+    else{
+        printf("The 2 numbers are not equal\n");
+    }
+}
